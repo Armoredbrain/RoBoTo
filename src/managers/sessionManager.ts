904611a29d2c05eb70a52e75126e5be8b35da872
tardigrade <benjamin.jaud@neomanis.io>
2023-05-17 17:17:08 +0200

    :fire: remove old logic not applicable here

--- src/managers/sessionManager.ts
+++ src/managers/sessionManager.ts
@@ -1,34 +1,14 @@
 import { SessionModel } from "../entities/Session";
-import { SessionStatus, Session } from "../types";
+import { Session } from "../types";
 
 export async function sessionBuilder(session: Partial<Session>): Promise<Session> {
     const userSession = new SessionModel(session);
 
-    return await userSession.save();
+    return (await userSession.save()).toObject();
 }
 
-export async function getSessionById(id: string): Promise<Session> {
-    return await SessionModel.findOne({ _id: id }).orFail();
-}
-
-export async function getSessionByTicketUid(ticketUid: string): Promise<Session | undefined | null> {
-    return await SessionModel.findOne({ "ticket.uid": ticketUid });
-}
-
-/**
- *  It will get an available session without ticket
- */
-export async function getAvailableSessionByNeoId(userNeoId: number): Promise<Session | null> {
-    return await SessionModel.findOne(
-        {
-            status: SessionStatus.AVAILABLE,
-            ticket: { $exists: false },
-            userNeoId: userNeoId,
-            "history.0": { $exists: true },
-        },
-        {},
-        { created_at: -1 }
-    );
+export async function getSessionById(id: string): Promise<Session | undefined> {
+    return (await SessionModel.findOne({ _id: id }))?.toObject() ?? undefined;
 }
 
 export async function updateSession(session: Partial<Session>): Promise<void> {
--- src/tests/managers/sessionManager.test.ts
+++ src/tests/managers/sessionManager.test.ts
@@ -1,14 +1,7 @@
 import * as db from "../helper";
-import {
-    sessionBuilder,
-    sessionCleaner,
-    updateSessionTicket,
-    updateSession,
-    getSessionByTicketUid,
-    getAvailableSessionByNeoId,
-} from "../../managers/sessionManager";
-import { Session } from "../../schemas/interfaces";
+import { sessionBuilder, sessionCleaner, updateSession } from "../../managers/sessionManager";
 import { SessionModel } from "../../entities/Session";
+import { SessionStatus } from "../../types";
 
 beforeAll(async () => {
     await db.connect();
@@ -24,28 +17,20 @@ afterAll(async () => {
 
 describe("sessionBuilder", () => {
     test("Should create new session from partial session", async () => {
-        const session: Session = {
-            username: "toto",
-            userNeoId: 77,
-            computerName: "wiserthanme",
-            ticket: { uid: "GL-1" } as Partial<Ticket>,
+        const session = {
             flow: "ok",
             nextStep: { flow: "ok", id: 1 },
-        } as Session;
+            status: SessionStatus.AVAILABLE,
+            checkpoint: { flow: "ok", id: 1 },
+        };
         const newSession = await sessionBuilder(session);
 
         expect(newSession).toEqual(
             expect.objectContaining({
-                username: "toto",
-                userNeoId: 77,
-                computerName: "wiserthanme",
-                ticket: expect.objectContaining({ uid: "GL-1" }),
                 flow: "ok",
                 nextStep: { flow: "ok", id: 1 },
                 status: SessionStatus.AVAILABLE,
                 stacktrace: [],
-                techName: "neobot",
-                talkingToHuman: false,
                 history: [],
             })
         );
@@ -59,7 +44,6 @@ describe("updateSession", () => {
             username: "toto",
             userNeoId: 77,
             computerName: "wiserthanme",
-            ticket: { uid: "GL-1" } as Partial<Ticket>,
             flow: "ok",
             nextStep: { flow: "ok", id: 1 },
         };
@@ -104,10 +88,6 @@ describe("updateSession", () => {
 describe("sessionCleaner", () => {
     test("Should keep only keys pass in `keepKeys` argument", async () => {
         const session = {
-            username: "toto",
-            userNeoId: 77,
-            computerName: "wiserthanme",
-            ticket: { uid: "GL-1" } as Partial<Ticket>,
             flow: "ok",
             nextStep: { flow: "ok", id: 1 },
         };
@@ -127,13 +107,8 @@ describe("sessionCleaner", () => {
     });
     test("Should keep default keys in `keepKeys` argument", async () => {
         const session = {
-            username: "toto",
-            userNeoId: 77,
-            computerName: "wiserthanme",
-            ticket: { uid: "GL-1" } as Partial<Ticket>,
             flow: "ok",
             nextStep: { flow: "ok", id: 1 },
-            neoBotId: 55,
             status: 0,
         };
         const newSession = await sessionBuilder(session);
@@ -153,102 +128,5 @@ describe("sessionCleaner", () => {
             talkingToHuman: false,
             status: 0,
         });
-    });
-});
-
-describe("updateSessionTicket", () => {
-    test("Should update ticket in session based in `args` key", async () => {
-        const ticket = { uid: "GL-1" } as Partial<Ticket>;
-        const ticketUpdate = updateSessionTicket(ticket, {
-            name: "Problem printer",
-            content: "I have some trouble to find my printer",
-            resources: [
-                {
-                    item: { id: 4, name: "printer1", entity: { id: 1, name: "hello", itsmCode: "GL" } },
-                    type: "printer",
-                    tickets: [],
-                },
-            ],
-            userAssignedTo: [55],
-            userRequester: [],
-            status: Status.Assigned,
-            category: "phone",
-        });
-        expect(ticketUpdate).toEqual(
-            expect.objectContaining({
-                name: "Problem printer",
-                content: "<p>I have some trouble to find my printer</p>",
-                resources: [
-                    expect.objectContaining({
-                        item: { id: 4, name: "printer1", entity: { id: 1, name: "hello", itsmCode: "GL" } },
-                        type: "printer",
-                        tickets: [],
-                    }),
-                ],
-                userAssignedTo: [55],
-                status: Status.Assigned,
-                category: "phone",
-                uid: "GL-1",
-                userRequester: [],
-            })
-        );
-    });
-});
-
-describe("getSessionByTicketUid", () => {
-    test("should get session by ticket uid", async () => {
-        await sessionBuilder({
-            username: "toto",
-            userNeoId: 77,
-            computerName: "wiserthanme",
-            ticket: { uid: "GHI-456-JKL" } as Partial<Ticket>,
-            flow: "ok",
-            nextStep: { flow: "ok", id: 1 },
-        });
-        const session = await getSessionByTicketUid("GHI-456-JKL");
-        expect(session).toBeDefined();
-    });
-});
-
-describe("getAvailableSessionByNeoId", () => {
-    test("should get available and without ticket session by neo id and return it", async () => {
-        await sessionBuilder({
-            username: "toto",
-            userNeoId: 77,
-            computerName: "wiserthanme",
-            ticket: { uid: "GHI-789-JKL" } as Partial<Ticket>,
-            flow: "ok",
-            status: 0,
-            nextStep: { flow: "ok", id: 1 },
-        });
-        const sessionToGet = await sessionBuilder({
-            username: "toto",
-            userNeoId: 77,
-            computerName: "wiserthanme",
-            flow: "ok",
-            status: 0,
-            nextStep: { flow: "ok", id: 1 },
-            history: [
-                {
-                    content:
-                        "Pour information, les messages échangés dans ce chat seront enregistrés et consultables dans le ticket qui sera créé.",
-                    options: [],
-                    sender: 1,
-                    recipients: [2],
-                    isPrivate: false,
-                    sequencePosition: 1,
-                    createdAt: "2023-03-06T13:07:43.688Z",
-                    type: MessageType.MESSAGE,
-                },
-            ],
-        });
-        const session = await getAvailableSessionByNeoId(77);
-        expect(session).toBeDefined();
-        expect(session!.id).toEqual(sessionToGet.id);
-        expect(session!.ticket).toBeUndefined();
-    });
-    test("should fail to get available and without ticket session by neo id and return null", async () => {
-        const session = await getAvailableSessionByNeoId(123);
-        expect(session).toEqual(null);
     });
 });
