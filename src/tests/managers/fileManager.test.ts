b656f34d911d6809c44591b5495b4aeb9e781caa
tardigrade <benjamin.jaud@neomanis.io>
2023-05-22 12:24:16 +0200

    :fire: remove old tests cases not applicable to current robot state

--- src/tests/managers/fileManager.test.ts
+++
@@ -1,8 +0,0 @@
-import { fileReader } from "../../managers/fileManager";
-
-describe("fileReader", () => {
-    test("Should return json file as object", () => {
-        const flow = fileReader("toto");
-        expect(flow).toEqual({ name: "toto", license: "public", startingId: 1 });
-    });
-});
