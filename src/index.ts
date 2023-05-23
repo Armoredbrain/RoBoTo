6e8edc7685efd40abb87b9d302c45b6d97e3ac77
tardigrade <benjamin.jaud@neomanis.io>
2023-05-23 16:43:50 +0200

    :fire: remove https functionnality for now

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,38 @@
 import { app } from "./server";
 import logger from "./console/logger";
 import { connectToDB } from "./db/mongoDBConnection";
-import https from "https";
 import http from "http";
-import FS from "./wrapper/fs";
 import * as dotenv from "dotenv";
 import { connectToNlu } from "./managers/nlu";
 
 dotenv.config();
 
-const { NODE_ENV, LOCAL_URL, LOCAL_PORT, SERVICE_CRT, SERVICE_KEY, NLU_SERVER_URL, NLU_SECRET, SECRET } = process.env;
+const { NODE_ENV, LOCAL_URL, LOCAL_PORT, NLU_SERVER_URL, NLU_SECRET } = process.env;
 
 (async () => {
     try {
         if (NODE_ENV !== "test") {
-            if (!LOCAL_URL || !LOCAL_PORT || !NLU_SERVER_URL || !NLU_SECRET || !SERVICE_CRT || !SERVICE_KEY) {
+            if (!LOCAL_URL || !LOCAL_PORT || !NLU_SERVER_URL || !NLU_SECRET) {
                 throw new Error("Missing environment variables");
             }
             await connectToDB("chatbot");
             await connectToNlu();
 
-            http.createServer(
-                // {
-                //     rejectUnauthorized: true,
-                //     cert: FS.readFileSync(`./certificates/${SERVICE_CRT}`),
-                //     key: FS.readFileSync(`./certificates/${SERVICE_KEY}`),
-                //     requestCert: true,
-                //     passphrase: SECRET,
-                // },
-                app
-            ).listen(LOCAL_PORT, () => logger.info(`server running on port: ${LOCAL_PORT}`));
+            // TODO: implement a functionnal https server
+            // https
+            //     .createServer(
+            //         {
+            //             rejectUnauthorized: true,
+            //             cert: FS.readFileSync(`./certificates/${SERVICE_CRT}`),
+            //             key: FS.readFileSync(`./certificates/${SERVICE_KEY}`),
+            //             requestCert: true,
+            //             passphrase: SECRET,
+            //         },
+            //         app
+            //     )
+            //     .listen(LOCAL_PORT, () => logger.info(`server running on port: ${LOCAL_PORT}`));
+
+            http.createServer(app).listen(LOCAL_PORT, () => logger.info(`server running on port: ${LOCAL_PORT}`));
         }
     } catch (error) {
         logger.error(`Hu ho server says : ${error}`);
